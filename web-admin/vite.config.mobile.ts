c007e0968d45bfbcfaf31f8a48ef74cff5cb6e99
PHAM MINH TIEN <154680333+kennhope13@users.noreply.github.com>
2026-09-15 23:39:34 +0700

    release: v3.7.80 - Connect to Cloud Supabase for Mobile & Desktop

--- web-admin/vite.config.mobile.ts
+++ web-admin/vite.config.mobile.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   },
   define: {
     'import.meta.env.VITE_APP_VERSION': JSON.stringify(packageJson.version),
-    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('http://10.0.2.2:54321'),
-    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'),
+    'import.meta.env.VITE_SUPABASE_URL': JSON.stringify('https://nvdonaaxbtqjfmxtlgzb.supabase.co'),
+    'import.meta.env.VITE_SUPABASE_ANON_KEY': JSON.stringify('sb_publishable_gzUeVF_f2jadDuuii66pCw_W_0xmqjg'),
   },
 });
